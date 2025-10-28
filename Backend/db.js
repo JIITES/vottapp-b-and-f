9b67ddd437efdf9c1249c89f443c9ea750384309
jithesh <jitheshacharya48@gmail.com>
2025-10-28 13:52:02 +0530

    Initial commit for backend

--- Backend/db.js
+++ Backend/db.js
@@ -1,35 +1,53 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+// const mongoose = require('mongoose');
+// require('dotenv').config();
 
 
 // Connect to MongoDB you have to create your own link
-const MongodbURL=process.env.mongodbURL;
+// const MongodbURL=process.env.mongodbURL;
   
 //mandatory Set up to connection 
-mongoose.connect(MongodbURL,{
-    useNewUrlParser:true,
-    useUnifiedTopology: true 
+// mongoose.connect(MongodbURL,{
+//     useNewUrlParser:true,
+//     useUnifiedTopology: true 
 
-})
+// })
 
-//get the default connection
-//Mongoose maintains a default connection object representing the MongoDB connection 
-const db= mongoose.connection;
+// //get the default connection
+// //Mongoose maintains a default connection object representing the MongoDB connection 
+// const db= mongoose.connection;
 
-//Define event Listners for database connection  
-    db.on('connected',()=>{
-        console.log('Mongoose connected to Mongodb server');
-    })
+// //Define event Listners for database connection  
+//     db.on('connected',()=>{
+//         console.log('Mongoose connected to Mongodb server');
+//     })
 
-    db.on('error',()=>{
-        console.error('Mongoose connection error:');
-    });
+//     db.on('error',()=>{
+//         console.error('Mongoose connection error:');
+//     });
 
-    db.on('disconnected',()=>{
-        console.log('Mongoose disconnected from MongoDB server');
-    });
+//     db.on('disconnected',()=>{
+//         console.log('Mongoose disconnected from MongoDB server');
+//     });
 
-// Export the db connection for use in other files
-module.exports = db;
+// // Export the db connection for use in other files
+// module.exports = db;
 
 
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  console.error("MONGODB_URI not found in environment variables");
+  process.exit(1);
+}
+
+mongoose.connect(uri)
+  .then(() => console.log(" Connected to MongoDB Atlas"))
+  .catch(err => console.error(" MongoDB connection error:", err.message));
+
+
+
+    module.exports = db;
