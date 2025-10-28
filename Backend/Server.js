e5af984cf16c0cffd874793e70c8ec5af89600d1
jithesh <jitheshacharya48@gmail.com>
2025-10-28 18:06:49 +0530

    Add root route to show server running

--- Backend/Server.js
+++ Backend/Server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000 ;
 
 
 app.use(cors({
-    origin: CLIENT_URL || '*' ,// your React app URL
+    origin: CLIENT_URL || Client_url ,// your React app URL
     methods: ["GET", "POST", "PUT", "DELETE"],
 
     credentials: true
@@ -25,7 +25,7 @@ app.use(cors({
 app.use(bodyparser.json())
  
 app.get('/',(req,res)=>{
-    res.send('server is running');
+    res.send('server is running successfully, server ok ');
 })
 
 
